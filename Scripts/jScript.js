0b72541c1fca2c5342dce20c7e46d0d171e035e9
Abdella <abdellaalioncan@gmail.com>
2013-04-18 23:50:53 -0400

    changed some styling and created 'add' button
    Came across some interesting undesirable issues, will be fun to work
    through:
    
    1. When adding elements to the DOM, jquery styling does not apply to
    them, which makes sense
    2. Want to turn off the 'font' cursor when I hover over some text

--- Scripts/jScript.js
+++ Scripts/jScript.js
@@ -1,6 +1,39 @@
 $(document).ready(function(){
 
-$('#myList').sortable();
+var list = $('#myList');
+var listRows =$('#myList li');
 
+list.sortable();
+
+listRows.hover(hovered,hoveredOff);
+listRows.click(clicked);
+
+
+$('#addNew').click(addNewRow);
+
+
+
+
+
+
+
+//functions
+
+function hovered(){
+$(this).filter(':not(:animated)').toggleClass('hoverStyle', 300);
+}
+
+function hoveredOff(){
+$(this).removeClass('hoverStyle', 300);
+}
+
+function clicked() {
+$(this).toggleClass('clickStyle', 400);
+}
+
+function addNewRow() {
+
+$('#myList li:last').after('<li>Blank</li>');
+}
 
 });
