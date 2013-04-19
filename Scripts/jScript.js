25d19afdef012d5ed51d87b418f158de6804a9b9
Abdella <abdellaalioncan@gmail.com>
2013-04-19 18:47:55 -0400

    Working on tasklist input

--- Scripts/jScript.js
+++ Scripts/jScript.js
@@ -3,8 +3,16 @@ $(document).ready(function(){
 var list = $('#myList');
 var listRows =$('#myList li');
 
-list.sortable();
+//checks for keypresses
+var enterPressed = $(document).keypress(function(e) {
+if(e.which == 13) {
+console.log('Yay');
+}
+});
 
+
+
+list.sortable();
 list.on('mouseover','li', hovered);
 list.on('mouseout','li', hoveredOff);
 list.on('click','li',clicked);
@@ -12,13 +20,18 @@ list.on('click','li',clicked);
 
 $('#addNew').click(addNewRow);
 
-
-
-
+//$('#newTaskText').on('keypress', 'input', enterTask(event));
 
 
 
 //functions
+function enterTask(event) {
+console.log('something');
+if(event.which == 13) {
+alert("awesome, that was enter");
+}
+
+}
 
 function hovered(){
 $(this).filter(':not(:animated)').toggleClass('hoverStyle', 300);
@@ -34,7 +47,7 @@ $(this).toggleClass('clickStyle', 100);
 
 function addNewRow() {
 
-$('#myList li:last').after('<li>Blank</li>');
+$('#myList li:last').after('<li><input type="text" id="newTaskText" name"newTaskText"></input></li>');
 }
 
 });
