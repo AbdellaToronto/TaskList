b8ec22e48710c4bf6c137b465c4a40fa97fa2c78
Abdella <abdellaalioncan@gmail.com>
2013-04-19 21:51:16 -0400

    proper indentation

--- Scripts/jScript.js
+++ Scripts/jScript.js
@@ -1,59 +1,62 @@
-$(document).ready(function(){
-
-var list = $('#myList');
-var listRows =$('#myList li');
-
-//check for keypresses//
+$(document).ready(function() {
 
 
-list.sortable();
-list.on('mouseover','li', hovered);
-list.on('mouseout','li', hoveredOff);
-list.on('click','li',clicked);
+	//variables
+	var focusValue = 'default';
+	var list = $('#myList');
+	var listRows = $('#myList li');
+
+	//check for keypresses//
 
 
-$('#addNew').click(addNewRow);
-
-enterTask(checkIfTextSelected);
-
-
-
-
-//functions//
-function enterTask(checks) {
-
-var enterPressed = $(document).keypress(function(e) {
-if(e.which == 13) {
-console.log(checks);
-
-}
-});
+	list.sortable();
+	list.on('mouseover', 'li', hovered);
+	list.on('mouseout', 'li', hoveredOff);
+	list.on('click', 'li', clicked);
 
 
+	$('#addNew').click(addNewRow);
 
-}
+	enterTask(checkIfTextSelected);
 
-function checkIfTextSelected(){ //checks if cursor is in text area
-if($('.newTaskText').is(":focus")){
-return $('.newTaskText');
-}
-}
 
-function hovered(){
-$(this).filter(':not(:animated)').toggleClass('hoverStyle', 300);
-}
+	//functions//
 
-function hoveredOff(){
-$(this).removeClass('hoverStyle', 300);
-}
+	function enterTask(checks) {
+		
 
-function clicked() {
-$(this).toggleClass('clickStyle', 100);
-}
+		var enterPressed = $(document).keypress(function(e) {
+			if (e.which == 13) {
+				checks();
+				console.log(focusValue);
+			}
+		});
 
-function addNewRow() {
-$('#myList li:last').after('<li><input type="text" class="newTaskText" name"newTaskText"></input></li>');
-$('.newTaskText').focus();
-}
+
+	}
+
+	function checkIfTextSelected() { //checks if cursor is in text area
+
+		if ($('.newTaskText').is(":focus")) {
+			focusValue = $('.newTaskText:first').text();
+		}
+	}
+
+	function hovered() {
+		$(this).filter(':not(:animated)').toggleClass('hoverStyle', 300);
+	}
+
+	function hoveredOff() {
+		$(this).removeClass('hoverStyle', 300);
+	}
+
+	function clicked() {
+		$(this).toggleClass('clickStyle', 100);
+	}
+
+	function addNewRow() {
+		$('#myList li:last').after('<li><input type="text" class="newTaskText" name"newTaskText"></input></li>');
+		$('.newTaskText').focus();
+	}
 
 });
